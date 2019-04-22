202e90c5dfb34bcc1893f75344a424ce5ee4f6a0
ltvch <HrenHrenov@myrambler.ru>
2019-04-22 15:28:14 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAgregate
+{
+    class Program
+    {
+        enum Days
+        {
+            Rainy,
+            Stormy,
+            Sunny,
+            Cloudy,
+            Hot,
+            Cold,
+            Dry,
+            Wet,
+            Windy,
+            Hurricanes,
+            Typhoons,
+            Sand_storms,
+            Snow_storms,
+            Tornados,
+            Humid,
+            Foggy,
+            Snow,
+            Thundersnow,
+            Hail,
+            Sleet,
+            Drought,
+            Wildfire,
+            Blizzard,
+            Avalanche,
+            Mist
+        }
+
+        static void Main(string[] args)
+        {
+            string[][] arrDate = new string[2][];
+            arrDate[0] = new string[255];
+            arrDate[1] = new string[255];
+            arrDate[2] = new string[255];
+            arrDate[3] = new string[255];
+
+        }
+    }
+}
