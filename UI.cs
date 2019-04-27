e54d499b473c2e67de1cb5603737f59e1c970fc2
ltvch <HrenHrenov@myrambler.ru>
2019-04-27 04:33:28 +0300

    Update UI.cs

--- UI.cs
+++ UI.cs
@@ -1,16 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace WeatherOnArray
+namespace WeatherOnArrays
 {
     static class UI
     {
         public static void BuildTitle(string title)
         {
             Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine();
             Console.WriteLine(new string('#', title.Length + 4));
             Console.WriteLine($"# {title} #");
             Console.WriteLine(new string('#', title.Length + 4));
@@ -18,9 +14,13 @@ namespace WeatherOnArray
             Console.ForegroundColor = ConsoleColor.White;
         }
 
-        public static void StartMessage()
+        public static void ShowArray(string[][] WeathersInYears, int count)
         {
-            
+            Console.WriteLine("\nYou pressed Escape so see all value from Weather Agregator: ");
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Год {0} Месяц {1} День {2} Погода {3}", WeathersInYears[0][i], WeathersInYears[1][i], WeathersInYears[2][i], WeathersInYears[3][i]);
+            }
         }
     }
 }
